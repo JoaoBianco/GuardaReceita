4ccf8c100c928283ac4b98892a339dbe1af34ec3
JoaoBianco <joaops.bianco@gmail.com>
2020-11-23 14:53:31 -0300

    Botao Ver funcional

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ let criarNovaReceita = () => {
     bd.getProximoId(),
     nome_receita,
     descricao_receita,
-    modo_preparo,
-    ingredientes_receita
+    ingredientes_receita,
+    modo_preparo
   );
 
   bd.gravar(receita);
@@ -23,10 +23,20 @@ function criarBloco(receita) {
   div.classList = "col-md-4";
   div.innerHTML = `<div class="card mb-3"> <img src="https://via.placeholder.com/320"  
           class="card-img-top" alt="IMG"/><div class="card-body"> <h5 class="card-title">${receita.nome}</h5>
-           <p class="card-text">${receita.descricao}</p> <a href="#" class="btn-ver btn" data-toggle="modal" data-target="#exampleModal" >Ver</a>
+           <p class="card-text">${receita.descricao}</p> <a href="#" class="btn-ver btn" data-toggle="modal" data-target="#modal_apresentar" onclick="apresentarDetalhes(${receita.id})" >Ver</a>
            <a href="#" class="btn btn-secundarios">Editar</a><a href="#" class="btn btn-secundarios" id="receita_${receita.id}" onclick="remover(${receita.id})">Deletar</a></div></div>`;
   aplicacao.appendChild(div);
   limpar();
+}
+
+function apresentarDetalhes(id) {
+  let receita = localStorage.getItem(id);
+  receita = JSON.parse(receita);
+
+  document.getElementById("modal_apresentar_titulo").innerHTML = receita.nome;
+  document.getElementById("modal_ingredientes").innerHTML =
+    receita.ingredientes;
+  document.getElementById("modal_preparo").innerHTML = receita.modo_preparo;
 }
 
 function limpar() {
