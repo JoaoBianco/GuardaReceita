634453a92b371fe38e170d74ced9ec694d235683
JoaoBianco <joaops.bianco@gmail.com>
2020-11-24 16:10:00 -0300

    conclusao do projeto inicial

--- js/app.js
+++ js/app.js
@@ -21,10 +21,9 @@ function criarBloco(receita) {
   let aplicacao = document.getElementById("aplicacao");
   let div = document.createElement("div");
   div.classList = "col-md-4";
-  div.innerHTML = `<div class="card mb-3"> <img src="https://via.placeholder.com/320"  
-          class="card-img-top" alt="IMG"/><div class="card-body"> <h5 class="card-title">${receita.nome}</h5>
+  div.innerHTML = `<div class="card mb-3"> <div class="card-body"> <h5 class=" h2 pb-2 text-center border-bottom border-dark card-title">${receita.nome}</h5>
            <p class="card-text">${receita.descricao}</p> <a href="#" class="btn-ver btn" data-toggle="modal" data-target="#modal_apresentar" onclick="apresentarDetalhes(${receita.id})" >Ver</a>
-           <a href="#" class="btn btn-secundarios">Editar</a><a href="#" class="btn btn-secundarios" id="receita_${receita.id}" onclick="remover(${receita.id})">Deletar</a></div></div>`;
+           <a href="#" class="btn btn-secundarios" data-toggle='modal' data-target='#modal-editar' onclick="confirmarId(${receita.id})")">Editar</a> <a href="#" class="btn btn-secundarios" id="receita_${receita.id}" onclick="remover(${receita.id})">Deletar</a></div></div>`;
   aplicacao.appendChild(div);
   limpar();
 }
@@ -60,5 +59,37 @@ let listar = () => {
 
 function remover(id) {
   bd.remover(id);
+  listar();
+}
+
+function confirmarId(id) {
+  document
+    .getElementById("confirmar-editar-receita")
+    .setAttribute("onclick", `editarReceita(${id})`);
+  carregarDadosEditar(id);
+}
+
+function carregarDadosEditar(id) {
+  let receita = JSON.parse(localStorage.getItem(id));
+  document.getElementById("nome-editar-receita").value = receita.nome;
+  document.getElementById("descricao-editar-receita").value = receita.descricao;
+  document.getElementById("modo-preparo-editar-receita").value =
+    receita.modo_preparo;
+  document.getElementById("ingredientes-editar-receita").value =
+    receita.ingredientes;
+}
+
+function editarReceita(id) {
+  let receita = JSON.parse(localStorage.getItem(id));
+  receita.nome = document.getElementById("nome-editar-receita").value;
+  receita.descricao = document.getElementById("descricao-editar-receita").value;
+  receita.modo_preparo = document.getElementById(
+    "modo-preparo-editar-receita"
+  ).value;
+  receita.ingredientes = document.getElementById(
+    "ingredientes-editar-receita"
+  ).value;
+
+  localStorage.setItem(id, JSON.stringify(receita));
   listar();
 }
